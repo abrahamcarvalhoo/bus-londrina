572943fc8dc7717be967e3962743bf9c9ed9a0da
abrahamcarvalhoo <abraham.carvalhoo@gmail.com>
2018-02-05 03:09:14 -0200

    Small Updates #02

--- app/assets/javascripts/components/main.js
+++ app/assets/javascripts/components/main.js
@@ -4,11 +4,11 @@ app.controller('MainCtrl', function($scope, $http) {
   $scope.saved = localStorage.getItem('items');
   $scope.items = (localStorage.getItem('items') !== null) ? JSON.parse($scope.saved) : addItems($http);
 
-  $scope.loadMore = function() {
-    var last = $scope.items[$scope.items.length - 1];
-    for(var i = 1; i <= 8; i++) {
-      $scope.items.push(last + i);
-    }
+  $scope.ignoreAccents = function(item) {
+    if (angular.isObject(item)) return false;
+    var text = removeAccents(angular.lowercase('' + item));
+    var search = removeAccents(angular.lowercase('' + $scope.search));
+    return text.indexOf(search) > -1;
   };
 });
 
@@ -22,4 +22,26 @@ function addItems($http) {
   }, function(error) {
     console.log(error, 'can not get data.');
   });
+}
+
+function removeAccents(value) {
+  return value
+  .replace(/á/g, 'a')
+  .replace(/à/g, 'a')
+  .replace(/â/g, 'a')
+  .replace(/ã/g, 'a')
+  .replace(/é/g, 'e')
+  .replace(/è/g, 'e')
+  .replace(/ê/g, 'e')
+  .replace(/í/g, 'i')
+  .replace(/ï/g, 'i')
+  .replace(/ì/g, 'i')
+  .replace(/ó/g, 'o')
+  .replace(/ô/g, 'o')
+  .replace(/ú/g, 'u')
+  .replace(/ü/g, 'u')
+  .replace(/ç/g, 'c')
+  .replace(/ñ/g, 'n')
+  .replace(/õ/g, 'o')
+  .replace(/ß/g, 's');
 }
--- app/assets/javascripts/components/material.js
+++ app/assets/javascripts/components/material.js
@@ -1,8 +1,31 @@
 mdc.autoInit();
 
-var toggle = document.querySelector('.toggle');
+var toggleMenu = document.querySelector('.toggleMenu');
 var menu = new mdc.menu.MDCSimpleMenu(document.querySelector('#menu'));
 
-toggle.addEventListener('click', function() {
+toggleMenu.addEventListener('click', function() {
   menu.open = !menu.open;
+});
+
+if (document.querySelector('.mat-toolbar--open-search') && document.querySelector('.mat-toolbar--exit-search')) {
+  var searchIcon = document.querySelector('.mat-toolbar--open-search');
+  document.querySelector('.mat-toolbar--open-search').addEventListener('click', function() {
+    document.querySelector('.mat-toolbar--search').style = "visibility: visible; overflow: hidden; --mat-toolbar--search-location: " + (document.body.clientWidth - searchIcon.offsetLeft - 20) + "px;";
+    document.querySelector('.mat-toolbar--search-container').style = "animation: mat-toolbar--open-search 0.7s forwards; -webkit-transform: translateZ(0);";
+    setTimeout(function() {
+      document.querySelector('.mat-toolbar--search-text').focus();
+    }, 50);
+  });
+  document.querySelector('.mat-toolbar--exit-search').addEventListener('click', function() {
+    document.querySelector('.mat-toolbar--search-container').style = "animation: mat-toolbar--close-search 0.5s forwards; -webkit-transform: translateZ(0);";
+    setTimeout(function() {document.querySelector('.mat-toolbar--search').style = "visibility: hidden; --mat-toolbar--search-location: " + (document.body.clientWidth - searchIcon.offsetLeft - 20) + "px;";}, 500);
+  });
+}
+
+document.querySelector('.mat-search--clean').addEventListener('click', function() {
+  document.querySelector('.mat-search--desktop__input').focus();
+});
+
+document.querySelector('.mat-search--mobile__icon').addEventListener('click', function() {
+  document.querySelector('.mat-toolbar--search-text').focus();
 });
